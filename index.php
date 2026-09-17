2606cc9f178c727167cf1866f560893584811417
Eron Moquete <eronmoquete4@gmail.com>
2026-09-17 10:00:21 +0800

    Added Student display table on index.php

--- index.php
+++ index.php
@@ -29,6 +29,21 @@ $section = $_GET['section'] ?? 'students';
 // CRUD Operations
 $actions = $_GET['action'] ?? '';
 
+//----------------------------------
+//Students
+//----------------------------------
+
+//Fetch students
+if($section === 'students'){
+    $stmt = $pdo->query("
+        SELECT *
+        FROM students
+        ORDER by student_id
+        ");
+
+        $students = $stmt->fetchAll();
+}
+
 ?>
 
 <!DOCTYPE html>
@@ -47,7 +62,45 @@ $actions = $_GET['action'] ?? '';
     </nav>
     <hr>
     <?php if($section=='students'): ?>
-        <h2>Students</h2>
+    <h2>Students</h2>
+    <table>
+        <thead>
+            <tr>
+                <th>ID</th>
+                <th>First Name</th>
+                <th>Last Name</th>
+                <th>Course</th>
+                <th>Created At</th>
+                <th>Actions</th>
+            </tr>
+        </thead>
+        <tbody>
+            <?php foreach($students as $student): ?>
+            <tr>
+                <td>
+                    <?= htmlspecialchars($student['student_id']) ?>
+                </td>
+                <td>
+                    <?= htmlspecialchars($student['student_first_name']) ?>
+                </td>
+                <td>
+                    <?= htmlspecialchars($student['student_last_name']) ?>
+                </td>
+                <td>
+                    <?= htmlspecialchars($student['student_course']) ?>
+                </td>
+                <td>
+                    <?= htmlspecialchars($student['student_created_at']) ?>
+                </td>
+                <td>
+                    <a>Edit</a>
+                    |
+                    <a>Delete</a>
+                </td>
+            </tr>
+            <?php endforeach; ?>
+        </tbody>
+    </table>
     <?php endif; ?>
     
     <?php if($section=='books'): ?>
